133aa3aa227e5352ae40217b858d26a2af2fadb4
Anderson <andersonsurco5@gmail.com>
2021-10-02 12:15:13 -0500

    Añadiendo ProductoCrud y ProductoRepository

--- src/main/java/com/api/market/persistence/ProductoRepository.java
+++ src/main/java/com/api/market/persistence/ProductoRepository.java
@@ -1,2 +1,39 @@
-package com.api.market.persistence;public class ProductoRepository {
+package com.api.market.persistence;
+
+import com.api.market.persistence.crud.ProductoCrudRepository;
+import com.api.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll(){
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
 }
+
--- src/main/java/com/api/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/api/market/persistence/crud/ProductoCrudRepository.java
@@ -1,2 +1,12 @@
-package com.api.market.persistence.crud;public interface ProductoCrudRepository {
+package com.api.market.persistence.crud;
+
+import com.api.market.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
